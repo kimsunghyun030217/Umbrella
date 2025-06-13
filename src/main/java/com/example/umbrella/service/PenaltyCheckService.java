1b52de611ef322328b094abaf7cc0839f7a44c4d
Sunghyun Kim <youngbok2019334@ggh.goe.go.kr>
2025-06-13 14:07:53 +0900

    앱 개발과 연동완료(websocket제외)

--- src/main/java/com/example/umbrella/service/PenaltyCheckService.java
+++ src/main/java/com/example/umbrella/service/PenaltyCheckService.java
@@ -22,8 +22,9 @@ public class PenaltyCheckService {
         this.fcmService = fcmService;
     }
 
+    @Scheduled(cron = "0 0 9 * * *")
 
-    @Scheduled(cron = "0 0 1 * * *")
+    // @Scheduled(fixedRate = 60000) 1분마다 전송
     public void sendPenaltyNotification() {
         LocalDateTime now = LocalDateTime.now();
         List<User> overdueUsers = userRepository.findByPenaltyDueDateBefore(now);
